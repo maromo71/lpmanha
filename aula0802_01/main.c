a19fcfd496fe8f28356c395f232c47df05567541
maromo71m <professormoraes@gmail.com>
2021-02-08 08:56:35 -0300

    Primeiro commit

---
+++ aula0802_01/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Hoje e um dia \"especial\", voce esta em aula\n");
+    printf("\tO \\n pula uma linha \n");
+    printf("O desconto e de 5%% \n");
+
+    return 0;
+}
