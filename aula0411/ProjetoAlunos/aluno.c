c0ab4178c2e25ee1127d098fefd8e45f5b3205f0
Maromo71 <professormoraes@gmail.com>
2024-11-04 10:34:04 -0300

    Aula do dia 04 de novembro

--- aula0411/ProjetoAlunos/aluno.c
+++ aula0411/ProjetoAlunos/aluno.c
@@ -1,17 +1,53 @@
 #include "aluno.h"
 
-void inicializa() {
+#include <stdio.h>
+#include <stdlib.h>
 
+void inicializa() {
+    printf("Estrutura inicialiada\n");
+    for(int i=0; i<MAX; i++) {
+        v[i] = NULL;
+    }
 }
 
 void ler_dados(int pos) {
-
+    if(pos <0 || pos >= 100) {
+        printf("Erro: posicao invalida\n");
+        return;
+    }
+    v[pos] = (p_aluno) malloc(sizeof(struc Aluno));
+    printf("Digite os dados do aluno: \n");
+    fflush(stdin);
+    printf("Digite o nome do aluno: \n");
+    scanf(" %[^\n]s", v[pos]->nome);
+    fflush(stdin);
+    printf("Digite o codigo de matricula:\n");
+    scanf("%d", &v[pos]->codigo);
+    fflush(stdin);
+    printf("Digite o endereco do aluno:\n");
+    scanf(" %[^\n]s", v[pos]->endereco);
+    fflush(stdin);
+    printf("Digite o telefone do aluno:\n");
+    scanf(" %[^\n]s", v[pos]->telefone);
+    fflush(stdin);
+    printf("Digite a nota do aluno:\n");
+    scanf("%lf", &v[pos]->nota);
+    printf("DADOS CADASTRADOS COM SUCESSO\n\n");
 }
 
 void imprimir_unico(int pos) {
-
+    printf("Dados do Aluno Solicitado: \n");
+    printf("Nome: %s\n", v[pos]->nome);
+    printf("Maticula: %d\n", v[pos]->matricula);
+    printf("Endereco: %s\n", v[pos]->endereco);
+    printf("Telefone: %s\n", v[pos]->telefone);
+    printf("Nota: %s\n", v[pos]->nota);
+    printf("-----------------------------------\n\n");
 }
 
 void imprimir() {
-
+    for (int i=0; i<MAX; i++) {
+        if (v[i] == NULL) return; //força a saída
+        imprimir_unico(i);
+    }
 }
--- aula0411/ProjetoAlunos/main.c
+++ aula0411/ProjetoAlunos/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "aluno.h"
 
+p_aluno v[MAX];
 int main(void) {
     printf("Hello, World!\n");
     return 0;
