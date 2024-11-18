0169e18e7ad2ccd83703ccf4838fb242c3055113
Maromo71 <professormoraes@gmail.com>
2024-11-18 08:30:44 -0300

    Aula do dia 18 de novembro

--- aula1111/sampleInput/main.c
+++ aula1111/sampleInput/main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-
+//Teste:
+// oi - Entrada inválida
+// 100 - Valor lido 100
+// 100mma - Valor lido 100
 int main(void) {
     char buffer[101];
+    int valor;
     while(1) {
         printf("Digite um valor: \n");
         fgets(buffer, 101, stdin);
-
         //retirando o final "enter \n"
         buffer[strlen(buffer) - 1] = '\0';
-        printf("%s", buffer);
-
-        int valor;
-        //conversao segura
-        if(sscanf(buffer, "%d", &valor) == 1) {
+        if(sscanf(buffer, "%d", &valor) == 2) {
             break;
         }else {
             printf("\nEntrada inválida\n");
         }
     }
-
+    printf("Valor lido: %d \n", valor);
     return 0;
 }
