4e3bd2f8db072b98c1cf8c7186ecc9e92f95c7f5
maromo71 <professormoraes@gmail.com>
2025-02-10 09:43:10 -0300

    Aula do dia 10 de fevereiro

---
+++ aula1002/exemplo01/main.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+/**
+ *
+ * @param x Recebe o primeiro valor
+ * @param y Recebe o segundo valor
+ * @return Soma dos dois valores
+ */
+
+
+
+int main(void) {
+    //Imprimir na tela a palavra Hello World
+    printf("Hello, World!\n");
+
+    int a = 10;
+    int b = 30;
+
+
+    int soma = somar(a, b);
+
+    printf("Valor da soma %d \n", soma);
+    return 0;
+}
