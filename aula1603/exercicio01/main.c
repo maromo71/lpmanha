80b0194af13d625f45214396b0340eb895e63938
maromo71 <professormoraes@gmail.com>
2026-03-16 11:00:48 -0300

    Aula do dia 16 de marco

--- aula1603/exercicio01/main.c
+++ aula1603/exercicio01/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 //1. Validar a entrada do numero de voltas
-//2. Computador o tempo e fazer os calculos
+//2. Computar o tempo e fazer os calculos
 //   em um laço que contemple o numero de vio
 int main(void) {
-    printf("Hello, World!\n");
+    int n;
+    //1
+    do {
+        printf("Digite o numero de voltas [1..14]\n");
+        scanf("%d", &n);
+    }while (n<=0 || n>=15);
+    //2
+    double tempo_da_volta=0.0, melhor=0.0;
+    int melhor_volta = 0;
+    for (int i=1; i<=n; i++) {
+        printf("Digite o tempo da volta %d \n", i);
+        scanf("%lf", &tempo_da_volta);
+        if (i==1) {
+            melhor_volta = i;
+            melhor = tempo_da_volta;
+        }
+        if (tempo_da_volta < melhor) {
+            melhor = tempo_da_volta;
+            melhor_volta = i;
+        }
+    }
+    printf("Melhor tempo registrado: %.2lf \n", melhor);
+    printf("Volta em que ocorreu este tempo: %d \n", melhor_volta);
     return 0;
 }
