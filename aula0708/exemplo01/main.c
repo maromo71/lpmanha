7c7a202d277b22491eeea17ca868898c9bb2b050
Maromo <professormoraes@gmail.com>
2026-08-07 11:09:35 -0300

    Aula do dia 07 de agosto

--- aula0708/exemplo01/main.c
+++ aula0708/exemplo01/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+/* Minha mae
+ * Meu Tio
+ * Abacaxi
+ */
 
+/**
+ *
+ * @param p1 Primeira Nota do Aluno
+ * @param p2 Segunda Nota do Aluno
+ * @return Nédia aritmética normal do Aluno
+ */
+float media(float p1, float p2) {
+    return (p1 + p2)/2;
+}
 int main() {
+    //printf é o comando para impressao em tela
     printf("ola galera \n");
+    printf("Hoje esta um \"Lindo\" dia \n");
+    printf("Comprei 5%% do terreno \n");
+    printf("\t\t\t\t\tMenu");
     return 0;
 }
